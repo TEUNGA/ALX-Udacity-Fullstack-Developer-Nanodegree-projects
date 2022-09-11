7c8e64ea7a56426f6637af36bdfb7a2240c6c350
Aime <cesaime18@gmail.com>
2022-09-11 04:05:53 +0100

    db setup

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -12,7 +12,6 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
-  
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
